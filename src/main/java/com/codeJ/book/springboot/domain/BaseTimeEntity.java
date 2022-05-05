60ab8d6f0e375714c214ea3fa5d4f5e83a60391a
younjaeyeal <jyyoun1021@gmail.com>
2022-05-05 17:24:41 +0900

    게시판 등록,수정 삭제

---
+++ src/main/java/com/codeJ/book/springboot/domain/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.codeJ.book.springboot.domain;
+
+import lombok.Getter;
+import org.apache.tomcat.jni.Local;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+}
---
+++ src/main/java/com/codeJ/book/springboot/web/dto/PostsListResponseDTO.java
@@ -0,0 +1,22 @@
+package com.codeJ.book.springboot.web.dto;
+
+import com.codeJ.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDTO {
+
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDTO(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
